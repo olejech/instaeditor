c4b1b1d2b3e0d7e3603a0387b13472dfb88c05d6
gurenovich <gurenovich@mail.ru>
2019-06-18 19:00:47 +0300

    I'll make it better

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-container>
+    <nuxt />
+    <footer>
+      <div class="footer-copyright text-right py-3">© 2019 InstaEditor.ru
+        <nuxt-link to="/" class="text-secondary">Редактор</nuxt-link>
+        <nuxt-link to="/contact" class="text-secondary">Контакт</nuxt-link>
+        <nuxt-link to="/how" class="text-secondary">Инструкция</nuxt-link>
+      </div>
+    </footer>
+  </b-container>
+</template>
---
+++ pages/contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-jumbotron header="Контакт для связи" lead="Почта: hello@instaeditor.ru">
+      </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Контакт связи с автором InstaEditor',
+      meta: [
+        { hid: 'description', name: 'description', content: 'Связаться с автором можно с помощью электронной почты.' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
