069233cf468f2e410f991a342616dc4f1a0d3568
olejech <olejech@gmail.com>
2020-12-09 23:39:36 +0300

    rm links, get year dinamically

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,13 +5,12 @@
       <b-row class="py-3">
         <b-col>
           <nuxt-link to="/" class="text-secondary">Редактор</nuxt-link>
-          <nuxt-link to="/hashtags" class="text-secondary">Хештеги</nuxt-link>
           <nuxt-link to="/contact" class="text-secondary">Контакт</nuxt-link>
           <nuxt-link to="/how" class="text-secondary">Инструкция</nuxt-link>
         </b-col>
       </b-row>
       <b-row>
-        <b-col>© 2020 InstaEditor.ru</b-col>
+        <b-col>© {{ new Date().getFullYear() }} InstaEditor.ru</b-col>
       </b-row>
     </footer>
   </b-container>
