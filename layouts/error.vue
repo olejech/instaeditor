c9bffd1b86df892cce94e744196708cab1dd9867
olejech <olejech@gmail.com>
2020-12-10 12:42:50 +0300

    added 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center container">
+    <h1 v-if="error.statusCode === 404">Страница не найдена</h1>
+    <h1 v-else>An error occurred</h1>
+    Перейти на <NuxtLink to="/">главную страницу</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"]
+};
+</script>
+
+<style scoped>
+.container {
+  padding: 100px 0;
+}
+</style>
